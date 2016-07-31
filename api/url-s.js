1c231e1df6e5679d5d5bd7552935b9d1127e3142
Adithya J <adithya321@hotmail.com>
2016-07-31 18:54:05 +0530

    mongoDB Save Test

---
+++ api/url-s.js
@@ -0,0 +1,64 @@
+module.exports = function(app, db) {
+  app.get('/new/:url*', function(req, res) {
+    var url = req.url.slice(5);
+    var newUrlObj = {};
+    if(validURLRegex.test(url)) {
+      newUrlObj = {
+        "original_url": url,
+        "short_url": process.env.APP_URL
+      };
+      save(newUrlObj, db);
+    } else {
+      newUrlObj = {
+        "error": "Wrong url format"
+      };
+    }
+    res.send(newUrlObj);
+  });
+
+  function save(obj, db) {
+    var sites = db.collection('sites');
+    sites.save(obj, function(err, result) {
+      if (err) throw err;
+      console.log('Saved ' + result);
+    });
+  }
+};
+
+// Regex from https://gist.github.com/dperini/729294
+var validURLRegex = new RegExp(
+  "^" +
+    // protocol identifier
+    "(?:(?:https?|ftp)://)" +
+    // user:pass authentication
+    "(?:\\S+(?::\\S*)?@)?" +
+    "(?:" +
+      // IP address exclusion
+      // private & local networks
+      "(?!(?:10|127)(?:\\.\\d{1,3}){3})" +
+      "(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})" +
+      "(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})" +
+      // IP address dotted notation octets
+      // excludes loopback network 0.0.0.0
+      // excludes reserved space >= 224.0.0.0
+      // excludes network & broacast addresses
+      // (first & last IP address of each class)
+      "(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])" +
+      "(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}" +
+      "(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))" +
+      "|" +
+      // host name
+      "(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)" +
+      // domain name
+      "(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*" +
+      // TLD identifier
+      "(?:\\.(?:[a-z\\u00a1-\\uffff]{2,}))" +
+      // TLD may end with dot
+      "\\.?" +
+      ")" +
+    // port number
+    "(?::\\d{2,5})?" +
+    // resource path
+    "(?:[/?#]\\S*)?" +
+    "$", "i"
+    );
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var mongodb = require('mongodb');
+var api = require('./api/url-s.js');
 var app = express();
 var MongoClient = mongodb.MongoClient;
 var port = process.env.PORT || 8080;
@@ -12,75 +13,18 @@ MongoClient.connect(process.env.MONGOLAB_URI || url,
 		} else {
 			console.log('Successfully connected to MongoDB on port 27017.');
 		}
-		db.createCollection("sites", {
-			capped: true,
-			size: 5242880,
-			max: 5000
+
+		api(app, db);
+
+		app.get('/', function (req, res) {
+			res.sendFile(process.cwd() + '/public/index.html');
+		});
+
+		app.get('/new', function (req, res) {
+			res.sendFile(process.cwd() + '/public/index.html');
+		});
+
+		app.listen(port, function () {
+			console.log('Example app listening on port ' + port);
 		});
 	});
-
-app.get('/', function (req, res) {
-	res.sendFile(process.cwd() + '/public/index.html');
-});
-
-app.get('/new', function (req, res) {
-	res.sendFile(process.cwd() + '/public/index.html');
-});
-
-app.get('/new/:url*', function(req, res) {
-	var url = req.url.slice(5);
-	var newUrlObj = {};
-	if(validURLRegex.test(url)) {
-		newUrlObj = {
-			"original_url": url,
-			"short_url": process.env.APP_URL
-		};
-	} else {
-		newUrlObj = {
-			"error": "Wrong url format"
-		};
-	}
-	res.send(newUrlObj);
-});
-
-app.listen(port, function () {
-	console.log('Example app listening on port ' + port);
-});
-
-// Regex from https://gist.github.com/dperini/729294
-var validURLRegex = new RegExp(
-	"^" +
-    // protocol identifier
-    "(?:(?:https?|ftp)://)" +
-    // user:pass authentication
-    "(?:\\S+(?::\\S*)?@)?" +
-    "(?:" +
-      // IP address exclusion
-      // private & local networks
-      "(?!(?:10|127)(?:\\.\\d{1,3}){3})" +
-      "(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})" +
-      "(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})" +
-      // IP address dotted notation octets
-      // excludes loopback network 0.0.0.0
-      // excludes reserved space >= 224.0.0.0
-      // excludes network & broacast addresses
-      // (first & last IP address of each class)
-      "(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])" +
-      "(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}" +
-      "(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))" +
-      "|" +
-      // host name
-      "(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)" +
-      // domain name
-      "(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*" +
-      // TLD identifier
-      "(?:\\.(?:[a-z\\u00a1-\\uffff]{2,}))" +
-      // TLD may end with dot
-      "\\.?" +
-      ")" +
-    // port number
-    "(?::\\d{2,5})?" +
-    // resource path
-    "(?:[/?#]\\S*)?" +
-    "$", "i"
-    );
